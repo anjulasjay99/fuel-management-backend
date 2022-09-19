ab48e0383c2c887b93e3327869438d5cdec2b1df
anjulasjay99 <anjulasjay@gmail.com>
2022-09-19 18:07:32 +0530

    changes fuel order implementation

--- models/FuelOrder.js
+++ models/FuelOrder.js
@@ -3,12 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fuelOrderSchema = new Schema({
+  stationId: { type: String, required: true },
   refNo: { type: String, required: true },
   email: { type: String, required: true },
   type: { type: String, required: true },
   amount: { type: String, required: true },
   timeOfDelivery: { type: String, required: true },
   payment: { type: Number, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  province: { type: String, required: true },
+  zipCode: { type: Number, required: true },
+  contactNo: { type: Number, required: true },
   status: { type: String, required: true },
 });
 
