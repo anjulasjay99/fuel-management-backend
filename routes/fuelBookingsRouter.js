6a3d197570c9817c3793fa4f1611616c6d5a71dc
PraveenPeiris <praveenpeiris25@gmail.com>
2022-10-20 23:11:36 +0530

    fuel booking sprint 2 modifications

--- routes/fuelBookingsRouter.js
+++ routes/fuelBookingsRouter.js
@@ -1,39 +1,75 @@
 const router = require("express").Router();
 const fuelBooking = require("../models/FuelBookings");
 
-//fetch all feedbacks
-router.route("/").get(async (req, res) => {
-  await fuelBooking.find()
-    .then((data) => {
-      res.status(200).json({ status: true, data });
+//fetch all fuel bookings
+router.route("/:id").get(async (req, res) => {
+  const email = req.params.id;
+
+    await fuelBooking.find({ email })
+      .then((data) => {
+        res
+          .status(200)
+          .json({ status: true, msg: "Fetched Successfully", data });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ status: false, msg: "Error!" });
+      });
+});
+
+//create new fuel booking
+router.route("/").post(async (req, res) => {
+  const {
+    vehicleType,
+    vehicleNo,
+    bkgDate,
+    email,
+    litres,
+    fuelType,
+    stationName,
+    stationCity,
+  } = req.body;
+
+  const bookingId = "RES - " + Date.now().toString();
+  const status = "Pending";
+
+  const newFuelBkg = new fuelBooking({
+    bookingId,
+    orderDate: new Date().toISOString().split("T")[0],
+    vehicleType,
+    vehicleNo,
+    bkgDate,
+    email,
+    litres,
+    fuelType,
+    stationName,
+    stationCity,
+    status
+  });
+
+  newFuelBkg
+    .save()
+    .then(() => {
+      res.status(200).json({ status: true, msg: "Success" });
     })
     .catch((err) => {
       res.status(400).json({ status: false, error: err });
     });
 });
 
-//add new complaint
-router.route("/").post(async (req, res) => {
-  const { email, dateofComplaint, reason, complaintDetails} = req.body;
-  
-  const newfuelBooking = new fuelBooking({
-    email,
-    dateofComplaint,
-    reason,
-    complaintDetails,
-  });
-
-  newfuelBooking
-    .save()
-    .then((data) => {
-      res.status(200).json({ status: true, msg: "Success" });
-    })
-    .then((err) => {
-      res.status(400).json({ status: false, error: err });
-    });
+//get specific fuel booking by Id
+router.route("/get/:id").get(async(req,res) =>{
+  const id = req.params.id;
+  console.log(id);
+  await fuelBooking.findById(id).then((fuelBooking)=>{
+    res.json(fuelBooking);
+    console.log(fuelBooking);
+  }).catch((err) =>{
+    console.log(err);
+  })
 });
 
-//delete a complaint
+//delete a booking
 router.route("/delete/:id").delete((req, res) => {
     const id = req.params.id;
   
@@ -48,16 +84,19 @@ router.route("/delete/:id").delete((req, res) => {
       });
   });
 
-//update a complaint
+//update a booking
 router.route("/update/:id").put((req, res) => {
     const id = req.params.id;
     let data = new fuelBooking(req.body);
     fuelBooking
       .findOneAndUpdate(id, {
-        email: data.email,
-        dateofComplaint: data.dateofComplaint,
-        reason: data.reason,
-        complaintDetails: data.complaintDetails,
+        stationName: data.stationName,
+        stationCity: data.stationCity,
+        litres: data.litres,
+        bkgDate: data.bkgDate,
+        fuelType: data.fuelType,
+        vehicleNo: data.vehicleNo,
+        vehicleType: data.vehicleType,
       })
       .then(() => {
         res.status(200).json("Updated Successfully!");
