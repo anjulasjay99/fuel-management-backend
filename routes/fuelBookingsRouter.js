ae68739b91873664322f5c234a1e43e0e1fecc7c
PraveenPeiris <praveenpeiris25@gmail.com>
2022-10-21 11:34:07 +0530

    impl of fuel bkg request - station

--- routes/fuelBookingsRouter.js
+++ routes/fuelBookingsRouter.js
@@ -5,16 +5,16 @@ const fuelBooking = require("../models/FuelBookings");
 router.route("/:id").get(async (req, res) => {
   const email = req.params.id;
 
-    await fuelBooking.find({ email })
-      .then((data) => {
-        res
-          .status(200)
-          .json({ status: true, msg: "Fetched Successfully", data });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json({ status: false, msg: "Error!" });
-      });
+  await fuelBooking.find({ email })
+    .then((data) => {
+      res
+        .status(200)
+        .json({ status: true, msg: "Fetched Successfully", data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ status: false, msg: "Error!" });
+    });
 });
 
 //create new fuel booking
@@ -58,53 +58,53 @@ router.route("/").post(async (req, res) => {
 });
 
 //get specific fuel booking by Id
-router.route("/get/:id").get(async(req,res) =>{
+router.route("/get/:id").get(async (req, res) => {
   const id = req.params.id;
   console.log(id);
-  await fuelBooking.findById(id).then((fuelBooking)=>{
+  await fuelBooking.findById(id).then((fuelBooking) => {
     res.json(fuelBooking);
     console.log(fuelBooking);
-  }).catch((err) =>{
+  }).catch((err) => {
     console.log(err);
   })
 });
 
 //delete a booking
 router.route("/delete/:id").delete((req, res) => {
-    const id = req.params.id;
-  
-    fuelBooking
-      .findByIdAndDelete(id)
-      .then(() => {
-        res.status(200).json("Deleted Successfully!");
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json("Error!");
-      });
-  });
+  const id = req.params.id;
+
+  fuelBooking
+    .findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).json("Deleted Successfully!");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json("Error!");
+    });
+});
 
 //update a booking
 router.route("/update/:id").put(async (req, res) => {
-    const id = req.params.id;
-    let data = new fuelBooking(req.body);
-    
-    await fuelBooking.findByIdAndUpdate(id, {
-        stationName: data.stationName,
-        stationCity: data.stationCity,
-        litres: data.litres,
-        bkgDate: data.bkgDate,
-        fuelType: data.fuelType,
-        vehicleNo: data.vehicleNo,
-        vehicleType: data.vehicleType,
-      })
-      .then(() => {
-        res.status(200).json("Updated Successfully!");
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json("Error!");
-      });
-  });
+  const id = req.params.id;
+  let data = new fuelBooking(req.body);
+
+  await fuelBooking.findByIdAndUpdate(id, {
+    stationName: data.stationName,
+    stationCity: data.stationCity,
+    litres: data.litres,
+    bkgDate: data.bkgDate,
+    fuelType: data.fuelType,
+    vehicleNo: data.vehicleNo,
+    vehicleType: data.vehicleType,
+  })
+    .then(() => {
+      res.status(200).json("Updated Successfully!");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json("Error!");
+    });
+});
 
 module.exports = router;
